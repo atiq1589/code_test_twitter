2c324a77957f160095128c6d3117e7ffcaae7c7d
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2022-02-18 22:58:33 +0600

    added tweet model

---
+++ app/models/tweet.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from pydantic import BaseModel, constr
+
+
+class Tweet(BaseModel):
+    """
+    Tweet model 
+    Replicate DB record
+    """   
+    _TWEET_COUNT: int = 0 # will be replaced in future with real db
+    id: Optional[int] = None
+    user_id: int
+    body: constr(min_length=3, max_length=140)
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        if not self.id:
+            self.id = Tweet._TWEET_COUNT + 1
+            Tweet._TWEET_COUNT += 1
