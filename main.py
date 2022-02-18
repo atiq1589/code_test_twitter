644d1479ab9a1a7e08a145e47f612cbff5997d49
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2022-02-18 19:58:06 +0600

    added user authentication api

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from random import random
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, status, Depends, HTTPException
 from db import Database
-from models.users import User
+from models import User
+from models.forms import UserRegistrationModel
 from typing import List
-
+from authentication import get_password_hash, Token, authenticate_user, create_access_token, get_current_user
+from fastapi.security import OAuth2PasswordRequestForm
 
 app = FastAPI()
 
@@ -15,16 +17,34 @@ async def main():
 
 
 @app.post('/register', status_code=status.HTTP_201_CREATED)
-async def register(user: User, response: Response):
-    if IN_MEMORY_DB.find('users', username=user.username):
+async def register(user_form: UserRegistrationModel, response: Response):
+    if IN_MEMORY_DB.find('users', username=user_form.username):
         response.status_code = status.HTTP_400_BAD_REQUEST
         return dict(message="Username already found. Please choose a different username.")
     else:
-        IN_MEMORY_DB.insert('users', user.dict())
+        user = User(**user_form.dict())
+        user_dict = user.dict()
+        user_dict["hashed_password"] = get_password_hash(user_form.password)
+        IN_MEMORY_DB.insert('users', user_dict)
 
 
 @app.get('/users', response_model=List[User])
 async def getAllUsers():
     return IN_MEMORY_DB.find('users')
 
-    
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    return current_user
