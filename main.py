ec298f944949c1c865a407dfed496740b131da6f
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2022-02-18 19:20:25 +0600

    added registration api

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from fastapi import FastAPI
+from random import random
+from fastapi import FastAPI, Response, status
+from db import Database
+from models.users import User
+from typing import List
+
 
 app = FastAPI()
 
+IN_MEMORY_DB = Database()
 
 @app.get('/')
 async def main():
     return {'message': "This is a Simple Twitter Like App"}
+
+
+@app.post('/register', status_code=status.HTTP_201_CREATED)
+async def register(user: User, response: Response):
+    if IN_MEMORY_DB.find('users', username=user.username):
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return dict(message="Username already found. Please choose a different username.")
+    else:
+        IN_MEMORY_DB.insert('users', user.dict())
+
+
+@app.get('/users', response_model=List[User])
+async def getAllUsers():
+    return IN_MEMORY_DB.find('users')
+
     
