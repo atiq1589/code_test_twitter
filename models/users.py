f9cc978c0a7f960071000fe7281763e18d4f5072
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2022-02-18 19:57:21 +0600

    updated user model

--- models/users.py
+++ models/users.py
@@ -11,18 +11,9 @@ class User(BaseModel):
     id: Optional[int] = None
     name: str
     username: str
-    password: str
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         if not self.id:
             self.id = User._USER_COUNT + 1
             User._USER_COUNT += 1
-
-
-class UserLoginModel(BaseModel):
-    """
-    User login model
-    """
-    username: str
-    password: str
