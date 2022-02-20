e0c374965e944380cb9a07f31a5cb8cdcff1c1ba
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2022-02-20 15:20:14 +0600

    updated user model

--- app/models/users.py
+++ app/models/users.py
@@ -1,19 +1,24 @@
+from datetime import datetime
 from typing import Optional
-from pydantic import BaseModel
+import uuid
+from pydantic import BaseModel, Field
 
 
 class User(BaseModel):
     """
     User model 
-    Replicate DB record
     """   
-    _USER_COUNT: int = 2 # will be replaced in future with real db
-    id: Optional[int] = None
+    id: str = Field(default_factory=uuid.uuid4, alias="_id")
     name: str
     username: str
+    created_at: datetime = Field(default_factory=lambda: datetime.utcnow())
 
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        if not self.id:
-            self.id = User._USER_COUNT + 1
-            User._USER_COUNT += 1
+    class Config:
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "id": 'd2c64531-0a66-4efe-b34f-aa5f63d6251e',
+                "name": "Md. Atiqul Islam",
+                "username": "atiqul"
+            }
+        }
