3163eb34b3a37c180a087046897688d120238e22
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2022-02-20 15:20:06 +0600

    added mongo db connection class

---
+++ app/db/mongo.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+from typing import Any
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+class Mongo:
+    """
+    This class will provide Mongo connection as singleton instance
+    """
+    _instance = None
+   
+    def __init__(self) -> None:
+        # TODO: will replace with settings / environment 
+        self.client = AsyncIOMotorClient('mongodb://twitter_mongodb_router_01:27017')
+
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(cls._instance, cls):
+            cls._instance = super(Mongo, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __exit__(self, type, value, traceback):
+        self.client.close()
+    
+
+
+"""
+Consumer can directly use from this connection instance
+"""
+driver = Mongo()
+connection = driver.client
+
+def get_db(db_name:str = 'twitterDb'):
+    return connection[db_name]
