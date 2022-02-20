0a886a33c5521cbdcc709d1d8e4ff62f2aad3aea
Md. Atiqul Islam <atiqul.islam1589@gmail.com>
2022-02-20 16:21:28 +0600

    update tweet model

--- app/models/tweet.py
+++ app/models/tweet.py
@@ -1,21 +1,26 @@
+import uuid
 from datetime import datetime
 from typing import Optional
-from pydantic import BaseModel, constr
+
+from pydantic import BaseModel, Field, constr
 
 class Tweet(BaseModel):
     """
-    Tweet model 
-    Replicate DB record
+    Tweet model this model sharded by user_id in DB
+    Be careful changing this model
     """   
-    _TWEET_COUNT: int = 0 # will be replaced in future with real db
-    id: Optional[int] = None
-    user_id: int
+    id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    user_id: uuid.UUID # BECAREFUL TO CHANGE THIS FIELD
     body: constr(min_length=3, max_length=140)
     created_at: datetime = None
 
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        if not self.id:
-            self.id = Tweet._TWEET_COUNT + 1
-            Tweet._TWEET_COUNT += 1
-            self.created_at = datetime.utcnow()
+    class Config:
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "id": 'd2c64531-0a66-4efe-b34f-aa5f63d6251e',
+                "user_id": '80bb1a41-ccd9-45b2-b949-d2c79584a6d7',
+                "body": "my tweet",
+                "created_at": "2022-02-20T09:18:44.625024"
+            }
+        }
